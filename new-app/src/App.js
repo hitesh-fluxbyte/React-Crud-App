ff48723dcb3071b5f1319509cfca04abebd35336
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-14 17:07:48 +0530

    Second-task

---
+++ new-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <div>
+          <h2>Welcome to React</h2>
+          <button class="btn btn-primary">Submit</button>
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
---
+++ new-app/src/components/header.js
@@ -0,0 +1,16 @@
+import ReactDom from "react-dom";
+
+function Message(props) {
+  {
+    return;
+    <h2> Component Example</h2>;
+  }
+  function App() {
+    return (
+      <div>
+        <Message username="root"></Message>
+      </div>
+    );
+  }
+}
+ReactDom.render(<mycomponent />, document.getElementById("root"));
