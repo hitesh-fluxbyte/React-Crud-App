3dd26c6fce3f82d688caf4a65b3e1d4d03439de9
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-17 15:18:52 +0530

    Changes

--- my-app-test/src/App.js
+++ my-app-test/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+// Function Based
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Hello React</h1>
+//     </div>
+//   );
+// }
+
+// Class Based
+class App extends React.Component{
+  render(){
+    return<h1 className="App">Hello React Application</h1>;
+  };
+};
 
 export default App;
-
- 
