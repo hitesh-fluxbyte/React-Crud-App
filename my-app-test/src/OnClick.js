e121e5c3113b71b633a419ee558827fcd85f362b
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-19 13:12:44 +0530

    OnClick

--- my-app-test/src/OnClick.js
+++ my-app-test/src/OnClick.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
+import React from "react";
 
+// Class Based
+// class Onclick extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//         Name:"ReactJs",
+//         Tech:"JavaScript"
+//     }
+//     // this.handleClick = this.handleClick.bind(this);
+//   }
+//   handleClick = () => {
+//     console.log("Button Clicked", this);
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <button className="btn btn-primary" onClick={this.handleClick}>
+//           Add Data
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
-class Onclick extends Component{
-    render(){
-        return (
-            <div className='App'>
-                <button className='btn btn-primary' onClick={this.handleClick}>Add Data</button>
-            </div>
-        );
-    };
+// export default Onclick;
+
+// Function Based
+function Onclick(props) {
+  function handleClick() {
+    console.log("Button Clicked");
+  }
+  return (
+    <div className="App">
+      <button className="btn btn-primary" onClick={handleClick}>
+        Add Data
+      </button>
+    </div>
+  );
 };
-
 export default Onclick;
