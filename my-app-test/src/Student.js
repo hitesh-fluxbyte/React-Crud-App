cede5dce5aa08af7edd2956bc91ac91bfdde2c04
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-19 10:18:44 +0530

    PropType Added

--- my-app-test/src/Student.js
+++ my-app-test/src/Student.js
@@ -1,6 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Student = (props) => {
   return <h1 className="App">Gujarat India {props.name}</h1>;
+};
+
+Student.prototype = {
+  name: PropTypes.string.isRequired,
+};
+Student.defaultProps = {
+  name: "Information Technologies",
 };
 export default Student;
