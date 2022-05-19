544cfc5b4b997122947ffc67edb0128ee364f880
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-19 11:40:14 +0530

    clean

--- my-app-test/src/State_With_Constructor.js
+++ my-app-test/src/State_With_Constructor.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 
 class StateWithConstructor extends Component {
-  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
     this.state = {
-        name:"Hitesh",
-        roll:11
-    }
-  };
-  render(){
-      return (
-        <h1 className="App">Hello {this.state.name}
-        Your Roll Number is {this.state.roll}</h1>
-      );
+      name: "Hitesh",
+      roll: 11,
+    };
+  }
+  render() {
+    return (
+      <h1 className="App">
+        Hello {this.state.name}
+        Your Roll Number is {this.state.roll}
+      </h1>
+    );
   }
 }
 export default StateWithConstructor;
